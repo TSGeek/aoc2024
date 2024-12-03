f7cb445f19be3ae849ddbcdeb3cc91389daf354e
Martin GRUDLER <devel@grudler.eu>
2024-12-03 08:21:03 +0100

    Refactoring of day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,26 +1,21 @@
 use std::fs;
 
-fn part_one(lc: &Vec<&str>, rc: &Vec<&str>) {
-    let mut left_col = lc.to_vec();
-    let mut right_col = rc.to_vec();
+fn part_one(left_col: &[i32], right_col: &[i32]) {
     let mut sum = 0;
-
-    left_col.sort();
-    right_col.sort();
     for i in 0..left_col.len() {
-        sum += (left_col[i].parse::<i32>().unwrap() - right_col[i].parse::<i32>().unwrap()).abs();
+        sum += (left_col[i] - right_col[i]).abs();
     }
-    println!("Part 1 -The sum is {}\n", sum);
+    println!("Part 1 - The sum is {}", sum);
 }
 
-fn part_two(left_col: &Vec<&str>, right_col: &Vec<&str>) {
+fn part_two(left_col: &[i32], right_col: &[i32]) {
     let mut sum = 0;
 
     for item in left_col {
-        sum += item.parse::<usize>().unwrap() * right_col.iter().filter(|&x| x == item).count();
+        sum += item * (right_col.iter().filter(|&x| x == item).count() as i32);
     }
 
-    println!("Part 2 -The sum is {}\n", sum);
+    println!("Part 2 - The sum is {}", sum);
 }
 
 fn main() {
@@ -28,10 +23,13 @@ fn main() {
     let mut left_col = Vec::new();
     let mut right_col = Vec::new();
     for line in content.lines() {
-        let values: Vec<&str> = line.split("   ").collect();
-        left_col.push(values[0]);
-        right_col.push(values[1]);
+        let mut values_iter = line.split("   ");
+        left_col.push(values_iter.next().unwrap().parse::<i32>().unwrap());
+        right_col.push(values_iter.next().unwrap().parse::<i32>().unwrap());
     }
+    left_col.sort();
+    right_col.sort();
+
     part_one(&left_col, &right_col);
     part_two(&left_col, &right_col);
 }
