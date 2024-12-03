14810e9d77dd54c42247dec8c90e0b567960b132
Martin GRUDLER <devel@grudler.eu>
2024-12-03 20:55:00 +0100

    Partial work of part 2 of day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,47 +1,85 @@
 use std::fs;
+use crate::IsSafe::{False, True};
 
-fn main() {
+fn phase_one() {
     let content = fs::read_to_string("inputs/day02.txt").expect("Failed to read input file.");
     let mut safe_count = 0;
     for line in content.lines() {
         let values: Vec<i32> = line.split(" ").map(|s| s.parse::<i32>().unwrap()).collect();
 
-        let mut is_increasing = false;
-        if values[0] < values[1] {
-            is_increasing = true;
-        }
-
-        let mut is_safe = false;
-        // On vérifie 2 choses :
-        // toujours croissant ou toujours décroissant (donc a < b ou a > b)
-        // la différence toujours <= 3 et >= 0
-
-        for i in 0..(values.len()-1) {
-            if is_increasing && values[i] < values[i+1] {
-                let diff = values[i+1] - values[i];
-                if (1..=3).contains(&diff) {
-                    is_safe = true;
-                } else {
-                    is_safe = false;
-                    break;
-                }
-            } else if !is_increasing && values[i] > values[i+1]  {
-                let diff = values[i] - values[i+1];
-                if (1..=3).contains(&diff) {
-                    is_safe = true;
-                } else {
-                    is_safe = false;
-                    break;
-                }
-            }
-            else {
-                is_safe = false;
-                break;
-            }
-        }
-        if is_safe {
-            safe_count += 1;
+        match check_safe(&values) {
+            IsSafe::True => safe_count += 1,
+            IsSafe::False(_) => (),
         }
     }
-    println!("Number of safe reports : {}", safe_count);
+    println!("Phase 1 - Number of safe reports : {}", safe_count);
+}
+
+fn phase_two() {
+    let content = fs::read_to_string("inputs/day02.txt").expect("Failed to read input file.");
+    let mut safe_count = 0;
+    for line in content.lines() {
+        let values: Vec<i32> = line.split(" ").map(|s| s.parse::<i32>().unwrap()).collect();
+
+        match check_safe(&values) {
+            True => safe_count += 1,
+            False(i) => {
+                let mut modified_values = values.clone();
+                modified_values.remove(i);
+                match check_safe(&modified_values) {
+                    True => safe_count += 1,
+                    False(_) => {
+                            let mut modified_values2 = values.clone();
+                            modified_values2.remove(i+1);
+                            match check_safe(&modified_values2) {
+                                IsSafe::True => safe_count += 1,
+                                IsSafe::False(_) => ()
+                            }
+                    }
+                }
+            }
+        }
+    }
+    println!("Phase 2 - Number of safe reports : {}", safe_count);
+}
+
+enum IsSafe<T> {
+    True,
+    False(T)
+}
+
+fn check_safe(values: &[i32]) -> IsSafe<usize> {
+    let mut is_increasing = false;
+    if values[0] < values[1] && values[1] < values[3] {
+        is_increasing = true;
+    } else if values[0] == values[1] {
+        return False(0)
+    }
+
+    // On vérifie 2 choses :
+    // toujours croissant ou toujours décroissant (donc a < b ou a > b)
+    // la différence toujours <= 3 et >= 0
+
+    for i in 0..(values.len() - 1) {
+        if is_increasing && values[i] < values[i + 1] {
+            let diff = values[i + 1] - values[i];
+            if !(1..=3).contains(&diff) {
+                return False(i);
+            }
+        } else if !is_increasing && values[i] > values[i + 1] {
+            let diff = values[i] - values[i + 1];
+            if !(1..=3).contains(&diff) {
+                return False(i);
+            }
+        } else {
+            return False(i);
+        }
+    }
+
+    True
+}
+
+fn main() {
+    phase_one();
+    phase_two();
 }
