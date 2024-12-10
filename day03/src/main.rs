78d34133043e2946a7a3a536f9b464d112c66d4a
Martin GRUDLER <devel@grudler.eu>
2024-12-10 09:19:58 +0100

    Code for part 1 of day 3, using regular expressions

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string("inputs/day03.txt").expect("Failed to read file");
+    let mut sum = 0;
+
+    let re = regex::Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+    for iter in re.captures_iter(&content) {
+        let (_, [a, b]) = iter.extract();
+        sum += a.parse::<i32>().expect("Failed to parse a.") * b.parse::<i32>().expect("Failed to parse b.");
+    }
+
+    println!("Result : {}", sum);
 }
